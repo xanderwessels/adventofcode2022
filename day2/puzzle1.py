6d971786fe2aba2b04a5bd28c46d827a05f06ed8
Xander Wessels <xanwes97@hotmail.com>
2022-12-04 16:58:56 +0100

    rename

--- day2/puzzle1.py
+++ day2/puzzle1.py
@@ -1,11 +1,31 @@
 from pathlib import Path
 f = open((Path(__file__).parent / "input.txt").resolve() , "r")
 
-choice_score = {'A X': 3+0, 'A Y': 1+3, 'A Z': 2+6, 'B X': 1+0, 'B Y': 2+3, 'B Z': 3+6, 'C X': 2+0, 'C Y': 3+3, 'C Z': 1+6}
+choice_score = {'X': 1, 'Y': 2, 'Z': 3}
+
+def score(line):
+    score = 0
+    score += choice_score[line[2]]
+    if line[0] == "A":
+        if line[2] == "Y":
+            score += 6
+        elif line[2] == "X":
+            score += 3
+    elif line[0] == "B":
+        if line[2] == "Z":
+            score += 6
+        elif line[2] == "Y":
+            score += 3           
+    elif line[0] == "C":
+        if line[2] == "X":
+            score += 6
+        elif line[2] == "Z":
+            score += 3                        
+    return score
 
 total_score = 0
 for line in f:
-    total_score += choice_score[line[0:3]]
+    total_score += score(line)
 
 f.close()
 print(total_score)
